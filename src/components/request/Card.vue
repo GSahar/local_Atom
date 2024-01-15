e8634e3b6b58be07221b66fa82da30969d9eb78e
Sheggy <horse741@ya.ru>
2024-01-15 19:41:44 +0500

    feat: Перенёс карточку заявки в компонет

--- src/components/request/Card.vue
+++ src/components/request/Card.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-
-  </div>
+   <v-card height="250px" width="300px">
+    <v-card-item >
+        <v-card-title>Заявка {{ card.number }}</v-card-title>
+          <div class="my-3 request-body" style="height: 125px;">
+            <p><strong>Инициатор заявки:</strong>  {{ card.author }}</p>
+            <p><strong>Приложение:</strong>  {{ card.task }}</p>
+            <p><strong>Стадия согласования:</strong>  {{ card.state }}</p>
+            <p><strong>Комментарий:</strong>  {{ card.comment }}</p>
+          </div>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary">Согласовать</v-btn>
+        </v-card-actions>
+    </v-card-item>
+  </v-card>
 </template>
 <script>
 export default {
-
+  name: 'MyRequestCard',
+  props: {
+    card: {
+      number: '',
+        author: '',
+        task: '',
+        state: '',
+        comment: ''
+    }
+  }
 }
 </script>
 <style lang="scss">
--- src/views/requests/Index.vue
+++ src/views/requests/Index.vue
@@ -2,32 +2,20 @@
   <v-container>
     <v-row>
       <v-col v-for="card in cards">
-        <v-card height="250px" width="300px">
-
-          <v-card-item >
-
-              <v-card-title>Заявка {{ card.number }}</v-card-title>
-                <div class="my-3 request-body" style="height: 125px;">
-                  <p><strong>Инициатор заявки:</strong>  {{ card.author }}</p>
-                  <p><strong>Приложение:</strong>  {{ card.task }}</p>
-                  <p><strong>Стадия согласования:</strong>  {{ card.state }}</p>
-                  <p><strong>Комментарий:</strong>  {{ card.comment }}</p>
-                </div>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary">Согласовать</v-btn>
-              </v-card-actions>
-        </v-card-item>
-        </v-card>
+        <my-request-card :card="card" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
-
+import MyRequestCard from '@/components/request/Card.vue';
 export default {
+  components:{
+    'my-request-card': MyRequestCard
+  },
   data() {
-   return { cards: [
+   return {
+    cards: [
       {
         number: '1',
         author: 'Иванов И.И.',
